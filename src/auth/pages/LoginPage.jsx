8fa142c4dc199920fcf0b5983f42e58340ca3ef3
Anthony Acosta M <anthony082008@hotmail.es>
2022-06-27 00:20:42 -0500

    App finished, ready for testing

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,11 +1,17 @@
 // import useNavigate
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context';
 
 export const LoginPage = () => {
+  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    navigate('/marvel', {
+    const lastPath = localStorage.getItem('lastPath') || '/';
+    login('Anthony Acosta');
+
+    navigate(lastPath, {
       replace: true,
     });
   };
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,9 +1,16 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../auth/context';
+
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
+  const { user, logout } = useContext(AuthContext);
+
   const navigate = useNavigate();
 
   const handleLogout = () => {
+    logout();
+
     navigate('/login', {
       replace: true,
     });
@@ -17,24 +24,15 @@ export const Navbar = () => {
 
       <div className="navbar-collapse">
         <div className="navbar-nav">
-          <NavLink
-            className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
-            to="/marvel"
-          >
+          <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="/marvel">
             Marvel
           </NavLink>
 
-          <NavLink
-            className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
-            to="/dc"
-          >
+          <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="/dc">
             DC
           </NavLink>
 
-          <NavLink
-            className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
-            to="/search"
-          >
+          <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="/search">
             Search
           </NavLink>
         </div>
@@ -42,7 +40,7 @@ export const Navbar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-info">Anthony</span>
+          <span className="nav-item nav-link text-info">{user?.name}</span>
 
           <button onClick={handleLogout} className="nav-item nav-link btn">
             Logout
